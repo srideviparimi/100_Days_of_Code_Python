8746b585940b5d08255ec47fe76d0113a8be4883
Sridevi <parimisridevi586@gmail.com>
2025-04-14 15:50:42 +1000

    first commit

---
+++ Day 1/task/task.py
@@ -0,0 +1,4 @@
+print("Welcome to bandname generator")
+city=input("What is the name of the city you grew up in?\n")
+pet=input("What pet do you have?\n")
+print("Your band name can be "+city+" "+pet)
